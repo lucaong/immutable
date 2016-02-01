8168e5ca5a81d4656e98a90a4b4463d06e6f3cd2
Luca Ongaro <lukeongaro@gmail.com>
2016-02-01 16:39:31 +0100

    implement and spec basic hash methods
    implement constructors, #fetch, #[], #set and #size

--- spec/immutable/hash_spec.cr
+++ spec/immutable/hash_spec.cr
@@ -3,5 +3,68 @@ require "json"
 
 describe Immutable do
   describe Immutable::Hash do
+    empty_hash = Immutable::Hash(String, Int32).new
+    hash       = Immutable::Hash.new({ "foo" => 1, "bar" => 2, "baz" => 3 })
+
+    describe ".new" do
+      it "creates an empty hash" do
+        h = Immutable::Hash(String, Int32).new
+        h.is_a?(Immutable::Hash(String, Int32)).should eq(true)
+        h.size.should eq(0)
+      end
+
+      it "with a block, it creates a hash with default" do
+        h = Immutable::Hash(String, Int32).new { |k| 42 }
+        h["xxx"].should eq(42)
+      end
+
+      it "with a hash, it creates and initialize an immutable hash" do
+        h = Immutable::Hash.new({ foo: 123 })
+        h[:foo].should eq(123)
+      end
+
+      it "with a hash and a block, it creates and initialize a hash with default" do
+        h = Immutable::Hash.new({ foo: 123 }) { |k| 42 }
+        h[:foo].should eq(123)
+        h[:xxx].should eq(42)
+      end
+    end
+
+    describe "#fetch" do
+      describe "with one argument and no block" do
+        it "returns value associated with key if it exists, else raise KeyError" do
+          hash.fetch("bar").should eq(2)
+          expect_raises(KeyError) { hash.fetch("xxx") }
+        end
+      end
+
+      describe "with one argument and a block" do
+        it "returns value associated with key if it exists, else eval block" do
+          hash.fetch("bar") { |k| k }.should eq(2)
+          hash.fetch("xxx") { |k| k }.should eq("xxx")
+        end
+      end
+
+      describe "with two arguments" do
+        it "returns value associated with key if it exists, else the default" do
+          hash.fetch("bar", 123).should eq(2)
+          hash.fetch("xxx", 123).should eq(123)
+        end
+      end
+    end
+
+    describe "#[]" do
+      it "returns value associated with key if it exists, else raise KeyError" do
+        hash["bar"].should eq(2)
+        expect_raises(KeyError) { hash["xxx"] }
+      end
+    end
+
+    describe "#[]?" do
+      it "returns value associated with key if it exists, else nil" do
+        hash["bar"].should eq(2)
+        hash["xxx"]?.should eq(nil)
+      end
+    end
   end
 end
--- src/immutable/hash.cr
+++ src/immutable/hash.cr
@@ -1,7 +1,88 @@
+# An `Immutable::Hash` is an immutable mapping of keys to values.
+#
+# An `Immutableble::Hash` can be constructed from a Crystal hash:
+#
+# ```
+# Immutable::Hash(Symbol, Int32).new      # => Hash {}
+# Immutable::Hash.new({ foo: 1, bar: 2 }) # => Hash {:foo => 1, :bar => 2}
+# ```
+#
+# `Immutable::Hash` works similarly to a standard Crystal hash, except it never
+# mutates in place, but rather return modified copies. However, this copies are
+# using structural sharing, ensuring performant operations and memory
+# efficiency. Under the hood, `Immutable::Hash` uses a bit-partitioned hash
+# trie, ensuring lookups and updates with a complexity of O(Log32), which for
+# practical purposes is equivalent to O(1): for hashes of viable sizes (even up
+# to billions of elements) the complexity is bound by a small constant.
+#
 require "./hash/trie"
 
 module Immutable
   struct Hash(K, V)
+    @trie  : Trie(K, V)
+    @block : (K -> V)?
+
+    def initialize(hash = {} of K => V : ::Hash(K, V))
+      @trie  = hash.reduce(Trie(K, V).empty) { |h, k, v| h.set(k, v) }
+      @block = nil
+    end
+
+    def initialize(hash = {} of K => V : ::Hash(K, V), &block : K -> V)
+      @trie  = hash.reduce(Trie(K, V).empty) { |h, k, v| h.set(k, v) }
+      @block = block
+    end
+
+    def initialize(@trie : Trie(K, V), @block = nil : (K -> V)?)
+    end
+
+    # Returns the number of key-value pairs in the hash
+    def size
+      @trie.size
+    end
+
+    # Returns the value associated with the given key, if existing, else raises
+    # `KeyError`
+    def fetch(key : K)
+      fetch(key) do
+        if b = @block
+          next b.call(key)
+        end
+        raise KeyError.new("Missing hash key: #{key.inspect}")
+      end
+    end
+
+    # Returns the value associated with the given key, if existing, else
+    # executes the given block and returns its value
+    def fetch(key : K, &block : K -> U)
+      @trie.fetch(key, &block)
+    end
+
+    # Returns the value associated with the given key, if existing, else
+    # it returns the provided default value
+    def fetch(key : K, default)
+      fetch(key) { default }
+    end
+
+    # Returns the value associated with the given key, if existing, else raises
+    # `KeyError`. See also `fetch`
+    def [](key : K)
+      fetch(key)
+    end
+
+    # Returns the value associated with the given key, if existing, else nil
+    def []?(key : K)
+      fetch(key, nil)
+    end
+
+    # Returns a modified copy of the hash where key is associated to value
+    #
+    # ```
+    # h  = Immutable::Hash.new({ foo: 123 })
+    # h2 = h.set(:bar, 321) # => Hash {:foo => 123, :bar => 321}
+    # h                     # => Hash {:foo => 123}
+    # ```
+    def set(key : K, value : V)
+      Hash.new(@trie.set(key, value), @block)
+    end
   end
 end
-
