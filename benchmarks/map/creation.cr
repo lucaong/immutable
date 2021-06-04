b1b699ad0c4fd201f1c25de4c16486ee5a5830b2
Luca Ongaro <mail@lucaongaro.eu>
2021-06-04 10:57:28 +0200

    code format

--- benchmarks/map/creation.cr
+++ benchmarks/map/creation.cr
@@ -2,7 +2,7 @@ Benchmark.ips do |b|
   banner "Map creation:"
 
   b.report("Hash creation") do
-    x = { :foo => 1 }
+    x = {:foo => 1}
 
     100.times {
       x = {:foo => 1, :bar => 2, :baz => 3, :qux => 4, :quux => 5}
@@ -10,7 +10,7 @@ Benchmark.ips do |b|
   end
 
   b.report("Map creation") do
-    x = { :foo => 1 }
+    x = {:foo => 1}
 
     100.times do
       x = Immutable.map({:foo => 1, :bar => 2, :baz => 3, :qux => 4, :quux => 5})
